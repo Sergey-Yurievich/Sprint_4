2a957b239aafbe12df6c8a0bc65857f7f4379c14
Sergey Kalinkin <italia80@yandex.ru>
2021-12-24 23:42:59 +0200

    Add regular expression

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,5 +1,8 @@
 import com.google.common.base.CharMatcher;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Account {
 
     private final String name;
@@ -8,18 +11,25 @@ public class Account {
         this.name = name;
     }
 
+    private static final String USERNAME_PATTERN =
+            "^(?=.{3,19}$)([a-zа-яёA-ZА-ЯЁ0-9]{1,19})+([\\s](?![\\s]))+([a-zа-яёA-ZА-ЯЁ0-9]{1,19})";
+
+    private static final Pattern pattern = Pattern.compile(USERNAME_PATTERN);
+
     public boolean checkNameToEmboss() {
 
-        int spaceCount = CharMatcher.is(' ').countIn(name);
-
-        if (spaceCount != 1 || name.startsWith(" ") || name.endsWith(" ")) {
-            return false;
-        } else if (name.length() >= 3 && name.length() <= 19) {
-            return true;
-        } else {
-            return false;
-        }
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
     }
+
+
+//        int spaceCount = CharMatcher.is(' ').countIn(name);
+//        if (spaceCount != 1 || name.startsWith(" ") || name.endsWith(" ")) {
+//            return false;
+//        } else if (name.length() >= 3 && name.length() <= 19) {
+//            return true;
+//        } else {
+
 }
 
 
